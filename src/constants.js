25ed077e30c4fa5882261fed68878c1beafd1ecb
ridz-shothikai <128675828+ridz-shothikai@users.noreply.github.com>
2025-04-21 09:51:31 +0600

    Update constants.js

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.reeqo.mongodb.net/eg_dg';
-export const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID || 'shothikai-gcp';
-export const GCS_BUCKET_NAME = process.env.GCS_BUCKET_NAME || 'temp-shothik';
-export const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY || 'AIzaSyDm4AuF7vILbSbwrT5hO0JlF3nq7UDozl0';
+export const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID || 'coherent-cinema-445604-m9';
+export const GCS_BUCKET_NAME = process.env.GCS_BUCKET_NAME || 'mybct';
+export const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY || 'AIzaSyAQcA4U7OnVgVRbr5MtlB6nPGdi2s2cyIc';
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET || 'YOUR_NEXTAUTH_SECRET_PLACEHOLDER';
