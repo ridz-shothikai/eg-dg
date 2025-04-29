8cd9f27b8016fb5f976cc92dd6cf9a65d15be57b
Riaduddin <rrriaduddin@gmail.com>
2025-04-29 12:29:56 +0600

    add the updated api value

--- src/constants.js
+++ src/constants.js
@@ -1,5 +1,5 @@
 export const MONGODB_URI = process.env.MONGODB_URI || 'mongodb+srv://admin:admin@cluster0.reeqo.mongodb.net/eg_dg';
 export const GOOGLE_CLOUD_PROJECT_ID = process.env.GOOGLE_CLOUD_PROJECT_ID || 'coherent-cinema-445604-m9';
 export const GCS_BUCKET_NAME = process.env.GCS_BUCKET_NAME || 'mybct';
-export const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY || 'AIzaSyAQcA4U7OnVgVRbr5MtlB6nPGdi2s2cyIc';
+export const GOOGLE_AI_STUDIO_API_KEY = process.env.GOOGLE_AI_STUDIO_API_KEY || 'AIzaSyA5cAf3IqBrMeZTDu8_1hnEpI_mM0X7gc4';
 export const NEXTAUTH_SECRET = process.env.NEXTAUTH_SECRET || 'YOUR_NEXTAUTH_SECRET_PLACEHOLDER';
